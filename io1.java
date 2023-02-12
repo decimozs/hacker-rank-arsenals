70ff4360229d136b54db949c6b0371021a2a751b
DecimoDevs <marlonadiguemartin548@gmail.com>
2023-02-12 18:00:48 +0800

    !hack

---
+++ io1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class solution {
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int c = scan.nextInt();
+
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+        }
+    }
+}
